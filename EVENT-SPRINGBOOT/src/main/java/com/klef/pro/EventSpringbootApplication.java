2b830b15c51afb7660b3e36ec2640b1e4e384300
2300031688 <2300031688@kluniversity.in>
2025-11-16 11:13:16 +0530

    intial commit

--- EVENT-SPRINGBOOT/src/main/java/com/klef/pro/EventSpringbootApplication.java
+++ EVENT-SPRINGBOOT/src/main/java/com/klef/pro/EventSpringbootApplication.java
@@ -2,10 +2,10 @@ package com.klef.pro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EventSpringbootApplication extends SpringBootServletInitializer {
+public class EventSpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EventSpringbootApplication.class, args);
